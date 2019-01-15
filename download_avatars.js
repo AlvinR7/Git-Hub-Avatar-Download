e47d267cb61b3655684a327ffead0a054b375e0b
AlvinR7 <36727415+AlvinR7@users.noreply.github.com>
2019-01-14 21:53:54 -0800

    Update download_avatars.js

--- download_avatars.js
+++ download_avatars.js
@@ -15,53 +15,19 @@ function getRepoContributors(repoOwner, repoName, cb) {
   };
 
   request(options, function(err, res, body) {
-    var data = JSON.parse(body); //parsing data
+    var data = JSON.parse(body); 
 
     cb(err, body,);
 
-    data.forEach(function(data) {    //iterate over results **referred to karl's notes
-      console.log(data.avatar_url);  //accessing github user content
+    data.forEach(function(data) {    
+      console.log(data.avatar_url);  
     })
     console.log(data.length + ' repos');
   });
 }
 
-// function getRepoContributors(repoOwner, repoName, cb) {
-//   var url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
-//   request(url, function(err, res, body) {
-//     cb(err, body);
-//   });
-// }  // ...
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
 });
-
-// getRepoContributors(err, result);
-
-
-
-
-// require('dotenv').config();
-
-// var request = require('request');
-
-// request({
-//   url: `https://api.github.com/users/jensen/repos`,
-//   qs: {
-//     sort: 'created',
-//     access_token: process.env.GITHUB_TOKEN
-//   },
-//   headers: {
-//     'user-agent': 'node application'
-//   }
-// }, function(error, response, body) {
-//   var repos = JSON.parse(body);
-
-//   repos.forEach(function(repo) {
-//     console.log(repo.name);
-//   });
-
-//   console.log(repos.length + ' repos');
-// });
